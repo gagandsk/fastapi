81fef3192aaf2a594859095e140ca7c1d7da7aa1
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-16 11:49:00 +0200

    Add Data and Model Validation in FastAPI Endpoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import zoneinfo
 from datetime import datetime
 from fastapi import FastAPI
-from models import Customer, Transaction, Invoice
+from models import Customer, CustomerCreate, Transaction, Invoice
 
 app = FastAPI()
 
@@ -24,9 +24,21 @@ async def time(iso_code: str):
     tz = zoneinfo.ZoneInfo(timezone_str)
     return {"time": datetime.now(tz)}
 
-@app.post('/customers')
-async def create_customer(customer_data: Customer):
-    return customer_data
+db_customers: list[Customer] = []
+
+@app.post('/customers', response_model=Customer)
+async def create_customer(customer_data: CustomerCreate):
+    customer = Customer.model_validate(customer_data.model_dump())
+    
+    # Ausmiendo que hace base de datos
+    customer.id = len(db_customers)
+    db_customers.append(customer)
+    return customer
+
+
+@app.get('/customers', response_model=list[Customer])
+async def list_customer():
+    return db_customers
 
 @app.post('/transactions')
 async def create_transaction(transaction_data: Transaction):
--- models.py
+++ models.py
@@ -1,11 +1,16 @@
 from pydantic import BaseModel
 
-class Customer(BaseModel):
-    id: int
+class CustomerBase(BaseModel):
     name: str
     description: str | None
     email: str
     age: int
+
+class CustomerCreate(CustomerBase):
+    pass
+
+class Customer(CustomerBase):
+    id: int | None = None
 
 class Transaction(BaseModel):
     id: int
