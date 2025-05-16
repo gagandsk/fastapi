44114771d036245168e7bdc489c36967ee5d5c6a
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-16 12:10:50 +0200

    Integrate SQLModel in FastAPI for Database Management

--- db.py
+++ db.py
@@ -1,12 +1,17 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import Session, create_engine
+from sqlmodel import Session, create_engine, SQLModel
+from fastapi import FastAPI
 
 sqlite_name = "db.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_name}"
 
 engine = create_engine(sqlite_url)
+
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
 
 def get_session():
     with Session(engine) as session:
