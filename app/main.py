23e3ebc5f4ab5da06be4e50d56e9f55ee79e746a
Gagandeep Dass Kaur <gagandeps0@gmail.com>
2025-05-19 10:40:40 +0200

    Implement Middleware in FastAPI to Measure Request Time

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import time
 import zoneinfo
 from datetime import datetime
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from db import create_all_tables
 from .routers import customers, transactions, invoices, plans
 
@@ -10,6 +11,14 @@ app.include_router(customers.router)
 app.include_router(transactions.router)
 app.include_router(invoices.router)
 app.include_router(plans.router)
+
+@app.middleware('http')
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    print(f"Request: {request.url} completed in: {process_time:.4f} seconds")
+    return response
 
 @app.get('/')
 async def root():
@@ -24,7 +33,7 @@ country_timezones = {
 }
 
 @app.get('/time/{iso_code}')
-async def time(iso_code: str):
+async def get_time_by_iso_code(iso_code: str):
     iso = iso_code.upper()
     timezone_str = country_timezones.get(iso)
     tz = zoneinfo.ZoneInfo(timezone_str)
